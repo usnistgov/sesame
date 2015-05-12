2833831d2002adeb615a853b70b8cc159f8ef347
Benoit <benoit.gaury@nist.gov>
2015-05-12 19:29:32 -0400

    modified small eps in observables

--- observables.py
+++ observables.py
@@ -21,10 +21,9 @@ def get_jn(efn, efnp1, v, vp1, dx, params):
     nC = params.nC
 
     h = 1e-30
-    lim = - exp(v)
 
     jn = exp(-bl) * (exp(efnp1) - exp(efn)) / dx * \
-         v*(1 - vp1/v + h*lim/v ) / (exp(-vp1)*(1 - exp(-v+vp1) + h*exp(vp1)))
+         (v - vp1 + h) / (-exp(-v)*(1 - exp(-vp1+v) + h))
     return jn * nC
 
 def get_jp(efp, efpp1, v, vp1, dx, params):
@@ -33,10 +32,9 @@ def get_jp(efp, efpp1, v, vp1, dx, params):
     nV = params.nV
 
     h = 1e-30
-    lim = - exp(-v)
     
     jp = exp(-eg + bl) * (exp(efpp1) - exp(efp)) / dx *\
-         v*(1 - vp1/v + h*lim/v ) / (exp(vp1)*(1 - exp(v-vp1) + h*exp(-vp1)))
+         (v - vp1 + h) / (-exp(v)*(1 - exp(vp1-v)  + h))
 
     return jp * nV
 
@@ -45,23 +43,20 @@ def get_jn_derivs(efn_i, efn_ip1, v_i, v_ip1, dx_i, params):
     nC = params.nC
 
     h = 1e-30
-    lim1 = - exp(v_i)
-    lim2 = - exp(v_i)/2
-    lim3 = - exp(v_i)/2
 
-    defn_i = 1./dx_i * exp(-bl+efn_i) * v_ip1 * (1 - v_i/v_ip1 + h*lim1/v_ip1)\
-             / (exp(-v_ip1)*(1 - exp(-v_i+v_ip1) + h/exp(-v_ip1)))
+    defn_i = 1./dx_i * exp(-bl+efn_i) * (v_ip1 - v_i + h)\
+             / (-exp(-v_i)*(1 - exp(-v_ip1+v_i) + h))
 
-    defn_ip1 = -1./dx_i * exp(-bl+efn_ip1) * v_ip1*(1 - v_i/v_ip1 + h*lim1/v_ip1)\
-               / (exp(-v_ip1) * (1 - exp(-v_i+v_ip1) + h/exp(-v_ip1)))
+    defn_ip1 = -1./dx_i * exp(-bl+efn_ip1) * (v_ip1 - v_i + h)\
+               / (-exp(-v_i) * (1 - exp(-v_ip1+v_i) + h))
 
     dv_i = -1./dx_i * exp(-bl) * (exp(efn_ip1) - exp(efn_i))\
-           * exp(-v_i)*(1+v_i-v_ip1 - exp(-v_ip1+v_i) + h*lim2/exp(-v_i))\
-           / (exp(-2*v_ip1) * ((1 - exp(-v_i+v_ip1))**2 + h/exp(-2*v_ip1)))
+           * exp(-v_i)*(1+v_i-v_ip1 - exp(-v_ip1+v_i) - h/2)\
+           / (exp(-2*v_i) * ((exp(-v_ip1-v_i)-1)**2 + h))
 
     dv_ip1 = -1./dx_i * exp(-bl) * (exp(efn_ip1) - exp(efn_i))\
-            * (exp(-v_ip1) * (1+v_ip1-v_i - exp(-v_i+v_ip1) + h*lim3/exp(-v_ip1)))\
-            / (exp(-2*v_ip1) * ((1 - exp(-v_i+v_ip1))**2 + h/exp(-2*v_ip1)))
+            * exp(-v_ip1) * (1+v_ip1-v_i - exp(-v_i+v_ip1) - h/2)\
+            / (exp(-2*v_ip1) * ((1-exp(v_ip1-v_i))**2 + h))
 
     return nC*defn_i, nC*defn_ip1, nC*dv_i, nC*dv_ip1   
 
@@ -71,24 +66,21 @@ def get_jp_derivs(efp_i, efp_ip1, v_i, v_ip1, dx_i, params):
     nV = params.nV
 
     h = 1e-30
-    lim1 = exp(-v_i)
-    lim2 = - exp(-v_i)/2
-    lim3 = - exp(-v_i)/2
 
-    defp_i = 1/dx_i * exp(bl-eg+efp_i) * v_ip1*(1 - v_i/v_ip1 + h*lim1/v_ip1)\
-             / (exp(v_ip1) * (1 - exp(v_i-v_ip1) + h/exp(v_ip1)))
+    defp_i = 1/dx_i * exp(bl-eg+efp_i) * (v_ip1 - v_i + h)\
+             / (-exp(v_i) * (1 - exp(v_ip1-v_i) + h))
 
-    defp_ip1 = -1/dx_i * exp(bl-eg+efp_ip1) * v_ip1*(1 - v_i/v_ip1 + h*lim1/v_ip1)\
-               / (exp(v_ip1) * (1 - exp(v_i-v_ip1) + h/exp(v_ip1)))
+    defp_ip1 = 1/dx_i * exp(bl-eg+efp_ip1) * (v_ip1 - v_i + h)\
+               / (exp(v_i) * (1 - exp(v_ip1-v_i) + h))
 
     dv_i = -1/dx_i * exp(bl-eg) * (exp(efp_ip1) - exp(efp_i))\
-           * (exp(v_i) * (1+v_ip1-v_i - exp(v_ip1-v_i) + h*lim2/exp(v_i)))\
-           / (exp(2*v_ip1)*((1 - exp(v_i-v_ip1))**2 + h/exp(2*v_ip1)))
+           * exp(v_i) * (1+v_ip1-v_i - exp(v_ip1-v_i) - h/2)\
+           / (exp(2*v_i)*((exp(v_ip1-v_i)-1)**2 + h))
 
     dv_ip1 = -1/dx_i * exp(bl-eg) * (exp(efp_ip1) - exp(efp_i))\
-            * (exp(v_ip1) * (1-v_ip1+v_i - exp(v_i-v_ip1) + h*lim3/exp(v_ip1)))\
-            / (exp(2*v_ip1)*((1-exp(v_i-v_ip1))**2 + h/exp(2*v_ip1)))
-           
+            * exp(v_ip1) * (1-v_ip1+v_i - exp(v_i-v_ip1) - h/2)\
+            / (exp(2*v_ip1)*((1-exp(-v_ip1+v_i))**2 + h))
+
     return nV*defp_i, nV*defp_ip1, nV*dv_i, nV*dv_ip1
 
 def get_rr_derivs(n, p, n1, p1, S, params):
