fd043753ac0033f38e71fffa9fc927245ec68e8f
Benoit <benoit.gaury@nist.gov>
2016-12-02 17:12:26 -0500

    3D example and added plane defects plot to the sesame scope

--- examples/3dpn.py
+++ examples/3dpn.py
@@ -1,5 +1,7 @@
 import sesame
 import numpy as np
+import cProfile
+import re
 
 
 # dimensions of the system
@@ -12,9 +14,10 @@ junction = 10e-9
 # Mesh
 x = np.concatenate((np.linspace(0,1.2e-6, 300, endpoint=False), 
                     np.linspace(1.2e-6, Lx, 100)))
-y = np.concatenate((np.linspace(0, 2.25e-6, 100, endpoint=False), 
+y = np.concatenate((np.linspace(0, 2.25e-6, 010, endpoint=False), 
                     np.linspace(2.25e-6, 2.75e-6, 100, endpoint=False),
                     np.linspace(2.75e-6, Ly, 100)))
+
 z = np.linspace(0, Lz, 100)
 
 sys = sesame.Builder(x, y, z)
@@ -25,27 +28,27 @@ def region(pos):
 
 # Add the donors
 nD = 1e17 * 1e6 # [m^-3]
-# print("add donor")
-# sys.add_donor(nD, region)
+print("add donor")
+sys.add_donor(nD, region)
 
 # Add the acceptors
 region2 = lambda pos: 1 - region(pos)
 nA = 1e15 * 1e6 # [m^-3]
-# print('add acceptor')
-# sys.add_acceptor(nA, region2)
+print('add acceptor')
+sys.add_acceptor(nA, region2)
 
 # Use perfectly selective contacts
 Sn_left, Sp_left, Sn_right, Sp_right = 1e50, 0, 0, 1e50
-# print('contacts')
-# sys.contacts(Sn_left, Sp_left, Sn_right, Sp_right)
+print('contacts')
+sys.contacts(Sn_left, Sp_left, Sn_right, Sp_right)
 
 # Dictionary with the material parameters
 reg1 = {'Nc':8e17*1e6, 'Nv':1.8e19*1e6, 'Eg':1.5, 'epsilon':9.4,
         'mu_e':200*1e-4, 'mu_h':200*1e-4, 'tau_e':10e-9, 'tau_h':10e-9, 
         'RCenergy':0, 'band_offset':0}
-# print("add material")
-# # Add the material to the system
-# sys.add_material(reg1)
+print("add material")
+# Add the material to the system
+sys.add_material(reg1)
 
 # gap state characteristics
 S = 1e5 * 1e-2           # trap recombination velocity [m/s]
@@ -53,11 +56,12 @@ E = -0.25                # energy of gap state (eV) from midgap
 N = 2e14 * 1e4           # defect density [1/m^2]
 
 # Specify the two points that make the line containing additional charges
-p1 = (20e-9, 2.5e-6, 100e-9)   #[m]
-p2 = (2.9e-6, 2.5e-6, 100e-9)  #[m]
+p1 = (1e-6, .5e-6, 1e-6)   #[m]
+p2 = (3.0e-6, .5e-6, 1e-6)  #[m]
 
-q1 = (20e-9, 3.5e-6, 1e-6)   #[m]
-q2 = (2.9e-6, 3.5e-6, 1e-6)  #[m]
+q1 = (1.0e-6, 4.5e-6, 1e-9)   #[m]
+q2 = (3.0e-6, 4.5e-6, 1e-9)  #[m]
+
 
 print('add plane defects')
 # Pass the information to the system
@@ -66,5 +70,4 @@ sys.add_plane_defects([p1, p2, q1, q2], E, N, S)
 
 sys.finalize()
 
-
-
+sesame.plot_plane_defects(sys)
