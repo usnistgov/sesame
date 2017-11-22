e332518adf80ccacb978450ac7dfa8cfb37c9e16
Benoit Gaury <benoitgaury@gmail.com>
2017-11-22 20:05:43 +0100

    typos in _abrupt functions

--- sesame/getF2_abrupt.py
+++ sesame/getF2_abrupt.py
@@ -32,7 +32,7 @@ def getF(sys, v, efn, efp, veq):
     ###########################################################################
     # carrier densities
     n = sys.Nc * np.exp(-sys.bl + efn + v)
-    p = sys.Nv * exp(-Eg + bl + efp - v)
+    p = sys.Nv * np.exp(-Eg + bl + efp - v)
 
     # equilibrium carrier densities
     n_eq = sys.Nc * np.exp(-sys.bl + veq)
