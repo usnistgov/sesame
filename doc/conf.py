65aac2144640fead3c8fc79e1d1a44a6728c5237
phaney2 <phaney@NIST.GOV>
2018-04-25 11:09:48 -0400

    trying again: changes to doc/conf.py for autodoc on readthedocs.org

--- doc/conf.py
+++ doc/conf.py
@@ -20,6 +20,8 @@
 import sys, os
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../sesame'))
+sys.path.insert(0, os.path.abspath('../sesame/'))
 
 # -- General configuration ------------------------------------------------
 
