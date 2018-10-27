d0a9aa37da6284db080a08622670ee2ae297e1eb
Paul Haney <paul.haney@nist.gov>
2018-10-27 14:18:47 -0400

    Update 1d_homojunction.py
    Removed debugging code and comments

--- examples/tutorial1/1d_homojunction.py
+++ examples/tutorial1/1d_homojunction.py
@@ -25,16 +25,14 @@ material = {'Nc':8e17, 'Nv':1.8e19, 'Eg':1.5, 'affinity':3.9, 'epsilon':9.4,
 sys.add_material(material)
 
 junction = 50e-7 # extent of the junction from the left contact [m]
-n_region = lambda pos: pos < junction
-p_region = lambda pos: pos >= junction
 
-#def n_region(pos):
-#    x = pos
-#    return x < junction
+def n_region(pos):
+    x = pos
+    return x < junction
 
-#def p_region(pos):
-#    x = pos
-#    return x >= junction
+def p_region(pos):
+    x = pos
+    return x >= junction
 
 # Add the donors
 nD = 1e17 # [cm^-3]
@@ -53,10 +51,6 @@ sys.contact_S(Sn_left, Sp_left, Sn_right, Sp_right)
 
 # First find the equilibrium solution
 solution = sesame.solve_equilibrium(sys)
-
-import matplotlib.pyplot as plt
-plt.plot(sys.xpts, sys.rho)#solution['v'])
-plt.show()
 
 # Define a function for the generation rate
 phi = 1e17         # photon flux [1/(cm^2 s)]
